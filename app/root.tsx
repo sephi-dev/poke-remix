968071cffa52230c6914abbab8f31c25c3e78e07
David <david@hostnfly.com>
2022-01-04 22:07:06 +0100

    chores: add tailwind to remix

---
+++ app/root.tsx
@@ -0,0 +1,41 @@
+import {
+  Links,
+  LinksFunction,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "remix";
+import type { MetaFunction } from "remix";
+import tailwindStyles from "./tailwind.css"
+
+export let links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: tailwindStyles}
+  ];
+};
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <title>Remix JS - starter</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
